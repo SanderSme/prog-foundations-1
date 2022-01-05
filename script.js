c51aa3f130032bc9919c5fe0e8087cae45a1090b
Sander Smedbol <sander.smedbol@stud.noroff.no>
2022-01-05 11:57:39 +0100

    Stuff

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Sander");
+console.log(4 + 4);
+console.log("the sum of 4 + 4 is equal:", 4 + 4);
+console.log(20 + 20, "hello world");
+
+//string
+var pet = "cat";
+console.log(pet);
+
+var petOne = "Cat";
+var petTwo = "cat";
+console.log(petOne == petTwo);
+
+var petThree = "cat";
+var petFour = "cat";
+console.log(petThree == petFour);
+
+var myPets = "my pets are " + petOne + " " + petTwo;
+console.log(myPets);
+
+var first = "rab";
+var second = "bit";
+var animal = first + second;
+console.log(animal);
+
+var four = "4";
+
+//numbers
+var integer = 4;
+var decimal = 5.5;
+console.log(integer * decimal);
+
+var multiply = integer * decimal;
+console.log(multiply);
+
+var devide = decimal / integer;
+console.log(devide);
+
+var firstNumber = 8;
+var secondNumber = 6;
+var sum = firstNumber + secondNumber;
+console.log(sum);
+
+//booleans
+var lightIsOn = false;
+console.log(lightIsOn);
+
+//data types
+var color = "red";
+typeof color;
+console.log(color);
